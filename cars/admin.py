9fb10d7c2c4f8ef089c2faf06736449a20ed1c73
Seyi' Daniel <dexter_daniel@outlook.com>
2023-03-15 08:52:45 +0100

    Featured cars, Latest Cars, and Image Gallery Implemented

--- cars/admin.py
+++ cars/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Car
+from typing import Any
 
 # Register your models here.
-admin.site.register(Car)
+
+class CarAdmin(admin.ModelAdmin):
+    def thumbnail(self, object) -> Any:
+        """this method displays the thumbnails of cars"""
+        return format_html('<img src="{}" width="40" style="border-radius: 10px;"/>'.format(object.car_photo.url))
+    
+    thumbnail.short_description = 'Thumbnail' #this attribute renames the thumbnail column to PHOTO
+    list_display: tuple = ('id', 'thumbnail', 'car_title', 'city', 'color', 'model', 'year', 'body_style', 'fuel_type', 'is_featured', 'condition', 'price')
+    list_display_links: tuple = ('id', 'thumbnail', 'car_title')
+    list_editable: tuple = ('is_featured',)
+    search_fields: tuple = ('id', 'car_title', 'city', 'model', 'body_style', 'fuel_type')
+    list_filter: tuple = ('city', 'model', 'body_style', 'fuel_type')
+admin.site.register(Car, CarAdmin)
--- pages/views.py
+++ pages/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from .models import Team
+from cars.models import Car
 
 # Create your views here.
 
 def home(request) -> render:
     teams: Team = Team.objects.all()
-
+    # featured_cars: Car = Car.objects.\
+    #     order_by('-created_date').\
+    #     filter(is_featured=True)
+    all_cars: Car = Car.objects.\
+        order_by('-created_date')
     data: dict = {
-        'teams': teams
+        'teams': teams,
+        'featured_cars': all_cars.filter(is_featured=True),
+        'all_cars': all_cars
     }
     return render(request, 'pages/home.html', data)
 
